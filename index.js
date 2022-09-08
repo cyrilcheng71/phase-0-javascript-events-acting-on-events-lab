87732e084438509a574b3bee468924ac656ea1b8
Man Cheng <cyrilcheng@protonmail.com>
2022-09-07 20:09:03 -0700

    commit tree 9cf141e8ca66d03777a31a97b09175f5f7a862e3

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Your code here
+
+
+document.getElementById("dodger").style.background = "#FF69B4";
+
+
+function moveDodgerLeft() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+  
+    if (left > 0) {
+      dodger.style.left = `${left - 1}px`;
+    }
+  }
+
+  
+  function moveDodgerRight(){
+    const rightNumbers = dodger.style.left.replace("px","");
+    const left = parseInt(rightNumbers, 10);
+    if (left < 360 ){
+        dodger.style.left = `${left + 1}px`;
+        }
+      }
+
+document.addEventListener("keydown", function (e) {
+    if (e.key === "ArrowLeft") {
+       moveDodgerLeft();
+    }else if (e.key === "ArrowRight"){
+         moveDodgerRight();
+    }
+  });
